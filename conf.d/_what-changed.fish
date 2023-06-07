55277b382f2ba604f83dae1ef1360df85cab8d82
kpbaks <kristoffer.pbs@tuta.io>
2023-06-07 20:30:21 +0200

    feat: add boilerplate

---
+++ conf.d/_what-changed.fish
@@ -0,0 +1,12 @@
+function _what-changed_install --on-event what-changed_install
+    # Set universal variables, create bindings, and other initialization logic.
+end
+
+function _what-changed_update --on-event what-changed_update
+	# Migrate resources, print warnings, and other update logic.
+end
+
+function _what-changed_uninstall --on-event what-changed_uninstall
+	# Erase "private" functions, variables, bindings, and other uninstall logic.
+end
+
