818801819ebcd1098263a41a559a668106bf1549
kpbaks <kristoffer.pbs@tuta.io>
2023-11-19 16:12:21 +0100

    refactor: use __what_changed.fish:: as prefix for functions

--- conf.d/_what-changed.fish
+++ conf.d/_what-changed.fish
@@ -1,36 +1,31 @@
-function _what-changed_install --on-event what-changed_install
-    # Set universal variables, create bindings, and other initialization logic.
-end
-
-function _what-changed_update --on-event what-changed_update
-    # Migrate resources, print warnings, and other update logic.
-end
-
-function _what-changed_uninstall --on-event what-changed_uninstall
-    # Erase "private" functions, variables, bindings, and other uninstall logic.
-end
-
 status is-interactive; or return
 
-set -g WHAT_CHANGED_MAXDEPTH 1
-set -g WHAT_CHANGED_VERBOSE 0
-set -g WHAT_CHANGED_USE_PREFIX 1
-set -g WHAT_CHANGED_DISABLED 0
+set --global WHAT_CHANGED_MAXDEPTH 1
+set --global WHAT_CHANGED_VERBOSE 0
+set --global WHAT_CHANGED_USE_PREFIX 1
+set --global WHAT_CHANGED_DISABLED 0
 # _what_changed_ is used as a namespace prefix for variables, to avoid collisions
-set -g _what_changed_directory_contents_before_command * .*
-set -g _what_changed_last_directory $PWD
-# set -g _what_changed_echo_prefix "[what-changed.fish]"
-set -g _what_changed_echo_prefix (printf "[%swhat%s-%schanged%s.%sfish%s]" (set_color green) (set_color normal) (set_color red) (set_color normal) (set_color yellow) (set_color normal))
+set --global _what_changed_directory_contents_before_command * .*
+set --global _what_changed_last_directory $PWD
+set --global _what_changed_echo_prefix (printf "[%swhat%s-%schanged%s.%sfish%s]" (set_color green) (set_color normal) (set_color red) (set_color normal) (set_color yellow) (set_color normal))
 
-function whatchanged
-    set -l options (fish_opt --short=h --long=help)
+function whatchanged --description "Manage what-changed.fish plugin"
+    set --local options (fish_opt --short=h --long=help)
     if not argparse $options -- $argv
         whatchanged --help
-        return 1
+        return 2
     end
 
+    set --local reset (set_color normal)
+    set --local green (set_color green)
+    set --local red (set_color red)
+    set --local blue (set_color blue)
+    set --local yellow (set_color yellow)
+    set --local bold (set_color --bold)
+
     if set --query _flag_help
-        set -l usage "$(set_color --bold)Interact with what-changed.fish$(set_color normal)
+        # TODO: use multiple printf instead
+        set --local usage "$(set_color --bold)Interact with what-changed.fish$(set_color normal)
 
 $(set_color yellow)Usage:$(set_color normal) $(set_color blue)$(status current-command)$(set_color normal) [options] on | off | status
 
@@ -46,15 +41,13 @@ $(set_color yellow)Options:$(set_color normal)
         return 0
     end
 
-    set -l argc (count $argv)
+    set --local argc (count $argv)
     if test $argc -eq 0
         whatchanged --help
         return 0
     end
 
-
-
-    set -l verb $argv[1]
+    set --local verb $argv[1]
     switch $verb
 
         case on
@@ -65,8 +58,8 @@ $(set_color yellow)Options:$(set_color normal)
             functions --erase _what_changed_preexec
             functions --erase _what_changed_postexec
         case status
-            set -l reset (set_color normal)
-            set -l color (test $WHAT_CHANGED_DISABLED -eq 1; and set_color green; or set_color red)
+            set --local reset (set_color normal)
+            set --local color (test $WHAT_CHANGED_DISABLED -eq 1; and set_color green; or set_color red)
             printf "\$WHAT_CHANGED_DISABLED:    %s%d%s\n" $color $WHAT_CHANGED_DISABLED $reset
             set color (test $WHAT_CHANGED_VERBOSE -eq 1; and set_color green; or set_color red)
             printf "\$WHAT_CHANGED_VERBOSE:     %s%d%s\n" $color $WHAT_CHANGED_VERBOSE $reset
@@ -74,6 +67,7 @@ $(set_color yellow)Options:$(set_color normal)
             printf "\$WHAT_CHANGED_USE_PREFIX:  %s%d%s\n" $color $WHAT_CHANGED_USE_PREFIX $reset
             printf "\$WHAT_CHANGED_MAXDEPTH:    %d\n" $WHAT_CHANGED_MAXDEPTH
         case "*"
+
             echo "Unknown verb: $verb"
             return 1
     end
@@ -83,37 +77,35 @@ if test $WHAT_CHANGED_DISABLED -eq 1
     return
 end
 
-function _what_changed_print_deleted_objects --argument-names deleted
-    set -l deleted $argv
+function __what_changed.fish::print_deleted_objects --argument-names deleted
+    set --local deleted $argv
+    set --local reset (set_color normal)
+    set --local green (set_color green)
+    set --local red (set_color red)
+
     if test $WHAT_CHANGED_VERBOSE -eq 1
         for it in $deleted
-            set_color red
-            echo "deleted file: $it"
-            set_color normal
+            printf "%sdeleted file: %s%s\n" $red $it $reset
         end
     else
-        set -l deleted_count (count $deleted)
-        test $deleted_count -eq 0; and return
+        set --local num_deleted (count $deleted)
+        test $num_deleted -eq 0; and return
         if test $WHAT_CHANGED_USE_PREFIX -eq 1
             printf "%s " $_what_changed_echo_prefix
         end
         printf "deleted%s %d%s %s\n" \
-            (set_color red) $deleted_count (set_color normal) \
-            (test $deleted_count -eq 1; and echo "file"; or echo "files")
+            $red $num_deleted $reset \
+            (test $num_deleted -eq 1; and echo "file"; or echo "files")
     end
 end
 
-function prompt_what_changed
-
-end
-
-function _what_changed_print_created_objects
-    set -l created $argv
+function __what_changed.fish::print_created_objects
+    set --local created $argv
     test (count $created) -eq 0; and return
     if test $WHAT_CHANGED_VERBOSE -eq 1
         for it in $created
-            set -l color
-            set -l prefix
+            set --local color
+            set --local prefix
             # Test for symlinks first, because they are also files
             if test -L $it
                 set color cyan
@@ -148,40 +140,40 @@ function _what_changed_print_created_objects
             printf "%s%s%s\n" (set_color $color) $prefix $it
         end
     else
-        set -l files
-        set -l directories
-        set -l symlinks
-        set -l pipes
-        set -l sockets
-        set -l block_devices
-        set -l character_devices
+        set --local files
+        set --local directories
+        set --local symlinks
+        set --local pipes
+        set --local sockets
+        set --local block_devices
+        set --local character_devices
 
         for it in $created
             # Test for symlinks first, because they are also files
             if test -L $it
-                set -a symlinks $it
+                set --append symlinks $it
             else if test -f $it
-                set -a files $it
+                set --append files $it
             else if test -d $it
-                set -a directories $it
+                set --append directories $it
             else if test -p $it
-                set -a pipes $it
+                set --append pipes $it
             else if test -S $it
-                set -a sockets $it
+                set --append sockets $it
             else if test -b $it
-                set -a block_devices $it
+                set --append block_devices $it
             else if test -c $it
-                set -a character_devices $it
+                set --append character_devices $it
             end
         end
 
-        set -l files_count (count $files)
-        set -l directories_count (count $directories)
-        set -l symlinks_count (count $symlinks)
-        set -l pipes_count (count $pipes)
-        set -l sockets_count (count $sockets)
-        set -l block_devices_count (count $block_devices)
-        set -l character_devices_count (count $character_devices)
+        set --local files_count (count $files)
+        set --local directories_count (count $directories)
+        set --local symlinks_count (count $symlinks)
+        set --local pipes_count (count $pipes)
+        set --local sockets_count (count $sockets)
+        set --local block_devices_count (count $block_devices)
+        set --local character_devices_count (count $character_devices)
 
         if test $WHAT_CHANGED_USE_PREFIX -eq 1
             printf "%s " $_what_changed_echo_prefix
@@ -189,7 +181,7 @@ function _what_changed_print_created_objects
 
         printf "created "
 
-        set -l reset (set_color normal)
+        set --local reset (set_color normal)
         if test $files_count -gt 0
             printf "%s%d%s %s " (set_color green) $files_count $reset (test $files_count -eq 1; and echo "file"; or echo "files")
         end
@@ -227,19 +219,20 @@ function _what_changed_postexec --on-event fish_postexec
         return # don't run if we've changed directories
     end
     # TODO: <kpbaks 2023-07-14 09:11:14> if a directory is created, then recursively check it
-    set -l directory_contents_after_prompt * .*
-    set -l deleted
+    set --local directory_contents_after_prompt * .*
+    set --local deleted
     for it in $_what_changed_directory_contents_before_command
-        set -l idx (contains --index -- $it $directory_contents_after_prompt)
+        set --local idx (contains --index -- $it $directory_contents_after_prompt)
         if test $status -eq 0
-            set -e directory_contents_after_prompt[$idx]
+            set --erase directory_contents_after_prompt[$idx]
             continue
         end
-        set -a deleted $it
+        set --append deleted $it
     end
 
-    _what_changed_print_deleted_objects $deleted
+    __what_changed.fish::print_deleted_objects $deleted
 
     # Will be empty if no files were deleted
-    _what_changed_print_created_objects $directory_contents_after_prompt
+    __what_changed.fish::print_created_objects $directory_contents_after_prompt
+
 end
